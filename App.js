15f58c44f95c367baae1c6860b0caa9ed8457c5e
naviret <111366578+naviret@users.noreply.github.com>
2024-02-22 11:21:11 -0500

    comment

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Hi my name is Ivan and I'm testing merge! </Text>
+      <Text> Another feature. </Text>
       <StatusBar style="auto" />
     </View>
   );
