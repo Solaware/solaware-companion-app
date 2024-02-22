2c14b5c36cdc99e4e5476933a1f7158d441d41bb
naviret <111366578+naviret@users.noreply.github.com>
2024-02-22 11:15:26 -0500

    testing merge

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Hello world! </Text>
+      <Text> Hi my name is Ivan and I'm testing merge! </Text>
       <StatusBar style="auto" />
     </View>
   );
