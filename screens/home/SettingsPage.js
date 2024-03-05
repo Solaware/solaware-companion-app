2138ffee0fec37a6704b44f179bad10074b0a0b1
vanshikasriram <100184381+vanshikasriram@users.noreply.github.com>
2024-03-05 10:36:22 -0500

    added tab organization

---
+++ screens/home/SettingsPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+import About from '../settings/AboutPage'
+import AcctSettings from '../settings/AcctSettingsPage'
+import HealthInfo from '../settings/HealthInfoPage'
+import NotifsManager from '../settings/NotifsManagerPage'
+import Sunscreen from '../settings/SunscreenPage'
+
+export default function SettingsPage() {
+    return (
+        <SafeAreaView>
+            <Text> settings </Text>
+        </SafeAreaView>
+    );
+}
