5ab25a0ea00d270036b1a339a4a768235162e68f
vanshikasriram <100184381+vanshikasriram@users.noreply.github.com>
2024-04-12 18:20:05 -0400

    addded fonts and text stylign

--- App.js
+++ App.js
@@ -2,14 +2,69 @@ import React from 'react'
 import Tabs from './screens/Tabs'
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, ImageBackground } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import {useFonts} from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+var image = require('./images/LoginPage.png');
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    'Tinos Regular': require('./assets/fonts/Tinos/Tinos-Regular.ttf'),
+    'Open Sans': require('./assets/fonts/Open_Sans/OpenSans-VariableFont_wdth,wght.ttf'),
+    'Tinos Bold': require('./assets/fonts/Tinos/Tinos-Bold.ttf'),
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading/>
+  }
+
   return (
-      <NavigationContainer>
-        <Tabs />
-      </NavigationContainer>
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
+
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+  },
+  TitleText: {
+    fontFamily: 'Tinos Bold', 
+    fontSize: 50,
+    color: '#08325D',
+    marginLeft: 42,
+    marginRight: 82,
+    marginTop: 30,
+  },
+  ButtonText: {
+    fontFamily: 'Open Sans', 
+    fontSize: 20,
+    color: 'white',
+  },
+  ButtonStyle: {
+    backgroundColor: '#08325D',
+    opacity: .9,
+    padding: 6,
+    borderRadius: 10,
+    display: 'flex',
+    width: 330,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+  }
+});
+
